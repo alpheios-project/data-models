3386ea69778a5bad421377ecb1abdedd22396d36
irina060981 <irina060981@yandex.ru>
2018-04-20 14:02:16 +1000

    create Translation class

---
+++ src/translation.js
@@ -0,0 +1,21 @@
+/**
+ * stores a scope of lemma translations from python service
+ * Contains a primary Lemma object
+ */
+class Translation {
+  /**
+   * Initializes a Translation object.
+   * @param {Lemma} lemma - A lemma object.
+   * @param [] meanings - A set of definitions.
+
+   */
+  constructor (lemma, meanings = []) {
+    if (!lemma) {
+      throw new Error('Lemma should not be empty.')
+    }
+
+    this.lemma = lemma
+    this.meanings = meanings
+  }
+
+}
