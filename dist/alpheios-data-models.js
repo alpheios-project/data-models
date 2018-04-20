5a0d618e17418eee6855e10e7d9cb23e7676bd96
irina060981 <irina060981@yandex.ru>
2018-04-20 16:22:38 +1000

    adding Translation property to Lemma

--- dist/alpheios-data-models.js
+++ dist/alpheios-data-models.js
@@ -2860,19 +2860,19 @@ class Translation {
     this.meanings = meanings;
   }
 
-  static loadTranslations (lemma, translationsList) {
+  static loadTranslations (lemma, itemTranslations) {
     // console.log('******** Translation class fetching translations', translationsList)
 
-    let res = [];
-    translationsList.forEach(function (item) {
-      // console.log('translationsList item ', item)
-      lemma.addTranslation(new Translation(lemma, item.translations));
-      res.push(lemma);
-    });
+    // let res = []
+    // translationsList.forEach(function (item) {
+    // console.log('translationsList item ', item)
+    lemma.addTranslation(new Translation(lemma, itemTranslations.translations));
+    // res.push(lemma)
+    // })
 
     // console.log('******** Translation class fetching translations 2 ', res)
 
-    return res
+    // return res
   }
 }
 
