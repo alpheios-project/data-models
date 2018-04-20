c5bd2d0949bfd15d2e51a38f815f0f3572296d26
irina060981 <irina060981@yandex.ru>
2018-04-20 14:12:20 +1000

    add Translation class to scope alpheios-data-models

--- dist/alpheios-data-models.js
+++ dist/alpheios-data-models.js
@@ -3380,5 +3380,26 @@ class ResourceProvider {
   }
 }
 
-export { constants as Constants, Definition, DefinitionSet, Feature, GrmFeature, FeatureType, FeatureList, FeatureImporter, Inflection, LanguageModelFactory, Homonym, Lexeme, Lemma, LatinLanguageModel, GreekLanguageModel, ArabicLanguageModel, PersianLanguageModel, ResourceProvider };
+/**
+ * stores a scope of lemma translations from python service
+ * Contains a primary Lemma object
+ */
+class Translation {
+  /**
+   * Initializes a Translation object.
+   * @param {Lemma} lemma - A lemma object.
+   * @param [] meanings - A set of definitions.
+
+   */
+  constructor (lemma, meanings = []) {
+    if (!lemma) {
+      throw new Error('Lemma should not be empty.')
+    }
+
+    this.lemma = lemma;
+    this.meanings = meanings;
+  }
+}
+
+export { constants as Constants, Definition, DefinitionSet, Feature, GrmFeature, FeatureType, FeatureList, FeatureImporter, Inflection, LanguageModelFactory, Homonym, Lexeme, Lemma, LatinLanguageModel, GreekLanguageModel, ArabicLanguageModel, PersianLanguageModel, ResourceProvider, Translation };
 //# sourceMappingURL=alpheios-data-models.js.map
