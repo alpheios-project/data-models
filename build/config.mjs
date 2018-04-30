758fb3e28b4e9befe131dde26744669dadc9ddad
Kirlat <kirlat@yula.media>
2018-04-30 13:09:04 +0400

    Minor updates to support latest version of node-build

--- build/config.mjs
+++ build/config.mjs
@@ -1,16 +1,15 @@
 const webpack = {
   common: {
+    entry: './driver.js',
     externals: {
       'intl-messageformat': 'intl-messageformat'
     }
   },
 
   production: {
-    entry: './driver.js',
     output: {filename: 'alpheios-data-models.min.js'}
   },
   development: {
-    entry: './driver.js',
     output: {filename: 'alpheios-data-models.js'}
   }
 }
