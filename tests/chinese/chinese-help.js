8cc55be84a967066c903df1babf15d2f9888b8d8
Irina <irina060981@yandex.ru>
2019-10-28 15:50:51 +1000

    create test chinese parsing

--- tests/chinese/chinese-help.js
+++ tests/chinese/chinese-help.js
@@ -1,4 +1,5 @@
 import 'whatwg-fetch'
+
 export default class ChineseHelp {
   static convertAdso (rawData) {
     let formattedData = new Map()
@@ -45,11 +46,11 @@ export default class ChineseHelp {
       let pinyin = parsedElement.substr(0, parsedElement.indexOf(']'))
       parsedElement = parsedElement.substr(parsedElement.indexOf(']') + 1)
 
-      let value = parsedElement.trim().substr(1)
-      value = value.substr(0, value.length - 1).replace(/\//g, '; ')
+      let shortDef = parsedElement.trim().substr(1)
+      shortDef = shortDef.substr(0, shortDef.length - 1).replace(/\//g, '; ')
 
       formattedData.set(currentIndex, {
-        word, pinyin, value
+        word, pinyin, shortDef
       })
     })
 
@@ -83,6 +84,33 @@ export default class ChineseHelp {
     return formattedData
   }
 
+  static convertHanzi (rawData) {
+    let formattedData = new Map()
+
+    // let data = rawData.slice(0, 10)
+    rawData.forEach(rawElement => {
+      let character = rawElement[0]
+      let typeCh = rawElement[1]
+      let typeText = rawElement[2]
+
+      let element = {
+        character
+      }
+      element[typeCh] = typeText
+
+      let prevElement = formattedData.get(rawElement[0])
+
+      if (prevElement) {
+        Object.keys(prevElement).filter(key => key !== 'character').forEach(key => {
+          element[key] = prevElement[key]
+        })
+      }
+      formattedData.set(character, element)
+    })
+    return formattedData
+    // console.info('formattedData - ', formattedData)
+  }
+
   static findWord (targetWord, wordIDX, wordDict) {
     let searchedIdxElement = wordIDX.get(targetWord)
 
@@ -98,5 +126,102 @@ export default class ChineseHelp {
     }
 
     return null
+  }
+
+  static lookupChinese (targetWord, dWordIndexSimp, dWordIndexTrad, dWordDict, dHanziDict) {
+    // console.info('lookupChinese dHanziDict - ', dHanziDict)
+    let cpWord = targetWord
+    let count = 0
+    let format = 'simp'
+
+    let rs = []
+
+    let result
+    while (cpWord.length > 1) {
+      // console.info('cpWord - ', cpWord)
+      if (format === 'simp') {
+        result = ChineseHelp.findWord(cpWord, dWordIndexSimp, dWordDict)
+        if (!result) {
+          format = 'trad'
+          result = ChineseHelp.findWord(cpWord, dWordIndexTrad, dWordDict)
+        }
+      } else {
+        result = ChineseHelp.findWord(cpWord, dWordIndexTrad, dWordDict)
+        if (!result) {
+          format = 'simp'
+          result = ChineseHelp.findWord(cpWord, dWordIndexSimp, dWordDict)
+        }
+      }
+
+      if (result) {
+        result.forEach(resItem => {
+          rs[count++] = resItem
+          rs[count - 1].format = format
+
+          ChineseHelp.formatDictionaryEntry(rs[count - 1])
+          ChineseHelp.formatCharacterInfo(rs[count - 1], dHanziDict)
+        })
+      }
+      cpWord = cpWord.substring(0, cpWord.length - 1)
+    }
+
+    return rs
+  }
+
+  static formatDictionaryEntry (resItem) {
+    if (resItem.format === 'trad') {
+      resItem.dictEntry = resItem.word.split(' ')[0]
+    } else if (resItem.format === 'simp') {
+      resItem.dictEntry = resItem.word.split(' ')[1]
+    }
+  }
+
+  static formatCharacterInfo (resItem, dHanziDict) {
+    // console.info('formatCharacterInfo dHanziDict - ', dHanziDict)
+    const freqName =
+      [
+        'least frequent',
+        'less frequent',
+        'moderately frequent',
+        'more frequent',
+        'most frequent'
+      ]
+
+    let unicode = ChineseHelp.unicodeInfo(resItem.dictEntry)
+    let hanziDatElement = dHanziDict.get(unicode)
+    // console.info('hanziDatElement - ', hanziDatElement)
+
+    if (hanziDatElement) {
+      // console.info('hanziDatElement inside')
+
+      if (hanziDatElement.kMandarin) {
+        resItem.mandarin = hanziDatElement.kMandarin
+      }
+      if (hanziDatElement.kDefinition) {
+        resItem.definition = hanziDatElement.kDefinition
+      }
+      if (hanziDatElement.kCantonese) {
+        resItem.cantonese = hanziDatElement.kCantonese
+      }
+      if (hanziDatElement.kTang) {
+        resItem.tang = hanziDatElement.kTang
+      }
+      if (hanziDatElement.kFrequency) {
+        resItem.frequency = freqName[hanziDatElement.kFrequency - 1]
+      }
+      if (hanziDatElement.kRSUnicode) {
+        resItem.unicode = hanziDatElement.kRSUnicode
+      }
+    }
+  }
+
+  static unicodeInfo (word) {
+    const hex = '0123456789ABCDEF'
+    const u = word.charCodeAt(0)
+    return 'U+' +
+        hex[(u >>> 12) & 15] +
+        hex[(u >>> 8) & 15] +
+        hex[(u >>> 4) & 15] +
+        hex[u & 15]
   }
 }
--- tests/chinese/chinese.test.js
+++ tests/chinese/chinese.test.js
@@ -5,11 +5,12 @@ import 'whatwg-fetch'
 import SIMPIDX from './json/simp-idx.json'
 import TRADIDX from './json/trad-idx.json'
 import ADSODAT from './json/adsolines.json'
+import HANZIDAT from './json/hanzi-dat.json'
 
 import ChineseHelp from './chinese-help.js'
 
 describe('chinese.test.js', () => {
-  let dWordIndexSimp, dWordIndexTrad, dWordDict, rawDict
+  let dWordIndexSimp, dWordIndexTrad, dWordDict, dHanziDict
 
   function timeNow () {
     return ((this.getHours() < 10) ? '0' : '') + this.getHours() + ':' + ((this.getMinutes() < 10) ? '0' : '') + this.getMinutes() + ':' + ((this.getSeconds() < 10) ? '0' : '') + this.getSeconds()
@@ -22,54 +23,13 @@ describe('chinese.test.js', () => {
     dWordIndexTrad = ChineseHelp.convertIDX(TRADIDX)
     dWordDict = ChineseHelp.convertAdso2(ADSODAT)
 
-    // const targetWord = '一举'
-    // let serchedIdxElement = dWordIndexSimp.get(targetWord)
-
-    // let searchedValue = 1319693
-    // let keys = Array.from(dWordDict.keys())
-
-    // console.info('targetWord', dWordIndexSimp.get(targetWord))
-    // console.info('searchedKey', serchedIdxElement.codes[0], dWordDict.get(serchedIdxElement.codes[0]))
+    dHanziDict = ChineseHelp.convertHanzi(HANZIDAT)
   })
 
   it('Chinese test - lookup prototype', () => {
     const targetWord = '一夫多妻主义者'
-    let cpWord = targetWord
-    let count = 0
-    let format = 'simp'
-    let lines
-
-    let rs = []
-
-    let result
-    while (cpWord.length > 1) {
-      console.info('cpWord - ', cpWord)
-      if (format === 'simp') {
-        result = ChineseHelp.findWord(cpWord, dWordIndexSimp, dWordDict)
-        // console.info('result simp - ', result)
-        if (!result) {
-          format = 'trad'
-          result = ChineseHelp.findWord(cpWord, dWordIndexTrad, dWordDict)
-          // console.info('result trad - ', result)
-        }
-      } else {
-        result = ChineseHelp.findWord(cpWord, dWordIndexTrad, dWordDict)
-        // console.info('result trad - ', result)
-        if (!result) {
-          format = 'simp'
-          result = ChineseHelp.findWord(cpWord, dWordIndexSimp, dWordDict)
-          // console.info('result simp - ', result)
-        }
-      }
-
-      if (result) {
-        result.forEach(resItem => {
-          rs[count++] = resItem
-        })
-      }
-      cpWord = cpWord.substring(0, cpWord.length - 1)
-    }
-
-    console.info('final', rs)
+    // console.info('test dHanziDict - ', dHanziDict)
+    const result = ChineseHelp.lookupChinese(targetWord, dWordIndexSimp, dWordIndexTrad, dWordDict, dHanziDict)
+    console.info('final', result)
   })
 })
